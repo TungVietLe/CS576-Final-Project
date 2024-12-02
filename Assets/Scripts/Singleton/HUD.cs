b2a2fdc7153478cecd42bc5443e862a0437e99b8
TungVLe <tunle0801@gmail.com>
2024-12-02 16:58:21 -0500

    HUD utilities added

---
+++ Assets/Scripts/Singleton/HUD.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : Singleton<HUD>
+{
+    [SerializeField] Image environmentHealth;
+
+    public void SetEnvironmentHealth(float val) {
+        if (val < 0 || val > 1) throw new Exception("health value should be between 0 and 1");
+        environmentHealth.fillAmount = val;
+    }
+
+    public void IncreaseEnvironmentHealth(float delta) {
+        SetEnvironmentHealth(environmentHealth.fillAmount + delta);
+    }
+
+    [SerializeField] TextMeshProUGUI missionDisplay;
+    private List<string> missions = new();
+    public void AddMission(string mission) {
+        missions.Add(mission);
+        updateMissionDisplay();
+    }
+    public void RemoveMission(string mission) {
+        int i = mission.IndexOf(mission);
+        if (i != -1) {
+            mission.Remove(i);
+        }
+        updateMissionDisplay();
+    }
+
+    private void updateMissionDisplay() {
+        missionDisplay.text = "";
+        string str = "";
+        missions.ForEach(e => str += "- e \n");
+    }
+
+
+}
