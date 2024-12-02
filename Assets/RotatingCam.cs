0d433e0bd1123b8b6c8bc10fa063e1494579bd38
TungVLe <tunle0801@gmail.com>
2024-12-02 15:14:28 -0500

    menu screen added

--- Assets/RotatingCam.cs
+++ Assets/RotatingCam.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RotatingCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float speed;
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
     }
 }
