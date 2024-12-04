a50b590ef68d4e72477ad751617aaac3738b86f1
Richard Nguyen <chiennguyen@umass.edu>
2024-12-03 20:17:18 -0500

    comment broken code

--- Assets/base-item/scripts/RedPill.cs
+++ Assets/base-item/scripts/RedPill.cs
@@ -1,17 +1,17 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class RedPill : BasePill
-{
-    [SerializeField] private float healAmount = 25f;
+// public class RedPill : BasePill
+// {
+//     [SerializeField] private float healAmount = 25f;
 
-    protected override void ActivateEffect()
-    {
-        base.ActivateEffect();
+//     protected override void ActivateEffect()
+//     {
+//         base.ActivateEffect();
         
-        PlayerHealth playerHealth = GetComponentInParent<PlayerHealth>();
-        if (playerHealth != null)
-        {
-            playerHealth.Heal(healAmount);
-        }
-    }
-} 
+//         PlayerHealth playerHealth = GetComponentInParent<PlayerHealth>();
+//         if (playerHealth != null)
+//         {
+//             playerHealth.Heal(healAmount);
+//         }
+//     }
+// } 
